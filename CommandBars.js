0ee4c45f659b610fc044783740f73252c81f498a
Nikhil Dabas <nikhil@nikhildabas.com>
2001-08-02 13:22:38 +0530

    Initial commit

---
+++ CommandBars.js
@@ -0,0 +1,99 @@
+/*
+ *  Command Bar Globals
+ *  Created by Nikhil Dabas ( ndabas@hotmail.com ).
+ */
+
+// Global Methods
+
+function SetStyleDefault(sPropName, sPropVal)
+{
+  style[sPropName] = (style[sPropName] == null || style[sPropName] == "")
+    ? sPropVal : style[sPropName];
+}
+
+function SetCustomStyleDefault(sCSSName, sScriptName, sDefault)
+{
+  if (currentStyle[sCSSName] == null)
+    style[sCSSName] = sDefault;
+  else
+    style[sCSSName] = currentStyle[sCSSName];
+  
+  style[sScriptName] = style[sCSSName];
+}
+
+function UpdatePrompt()
+{
+  if(Prompt)
+  {
+    var parts = Prompt.split("|");
+    Status = parts[0];
+    Title = parts[1];
+    title = parts[1];
+  }
+}
+
+function CancelEvent()
+{
+  event.cancelBubble = true;
+  event.returnValue = false;
+}
+
+// Global Constants
+
+CB_ACTIVE_CURSOR = "hand";
+
+CB_COLOR_BG = "#eee";
+CB_COLOR_FG = "#000";
+CB_COLOR_DISABLED = "#999";
+CB_FONT_FAMILY = "Tahoma, Verdana, Arial, sans-serif";
+
+CB_BAR_BORDER_DARK = "#666 solid 1px";
+CB_BAR_BORDER_LIGHT = "#ccc solid 1px";
+CB_BAR_HEIGHT = "29px";
+
+CB_STS_BAR_HEIGHT = "20px";
+
+CB_CTL_HEIGHT = "25px";
+CB_CTL_BORDER_NORMAL = "#eee solid 1px";
+CB_CTL_BORDER_ACTIVE = "#00c solid 1px";
+CB_CTL_COLOR_BG_NORMAL = "#eee";
+CB_CTL_COLOR_BG_ACTIVE = "#9cf";
+
+CB_INPUT_CTL_HEIGHT = "18px";
+CB_INPUT_CTL_BORDER_NORMAL = "#fff solid 1px";
+CB_INPUT_CTL_BORDER_ACTIVE = "#00c solid 1px";
+CB_INPUT_CTL_COLOR_BG = "#fff";
+
+CB_MNU_BORDER = "#666 solid 1px";
+CB_MNU_COLOR_SEPERATOR = "#666";
+
+CB_MNUI_COLOR_BG_NORMAL = "#fff";
+CB_MNUI_COLOR_BG_ACTIVE = "#9cf";
+CB_MNUI_IMAGE_EMPTY = "images\\trans.gif";
+CB_MNUI_IMAGE_CHECKED = "images\\check.gif";
+
+CB_FILTER_SHADOW = "progid:DXImageTransform.Microsoft.dropShadow(" +
+  " Color=999999,offX=3,offY=3,positive=true)";
+CB_FILTER_DISABLED = "progid:DXImageTransform.Microsoft.BasicImage(" +
+  " Rotation=0,Mirror=0,Invert=0,XRay=0,Grayscale=1,Opacity=1.00)";
+CB_FILTER_IMAGE_DISABLED = "progid:DXImageTransform.Microsoft.Alpha(" +
+  " style=0,opacity=50)";
+
+// Debug
+
+var Debugging = true;
+
+function TRACE(sMessage)
+{
+  if(!Debugging)
+    return;
+  
+  if(!window.CBDebugWnd || window.CBDebugWnd.closed)
+  {
+    var sWndProps = "toolbar=0,location=0,directories=0,status=0,";
+    sWndProps += "menubar=0,scrollbars=1,resizable=1,width=600,height=100";
+    window.CBDebugWnd = open ("Debug.htm", "Debug", sWndProps);
+  }
+  
+  window.CBDebugWnd.WriteMessage(sMessage);
+}
---
+++ ReadMe.txt
@@ -0,0 +1,15 @@
+Command Bars
+
+Created by Nikhil Dabas (ndabas@hotmail.com)
+
+Whatever you do, please include a line crediting the original author (that's me ;-))
+
+Here's a small description of the files included in the ZIP:
+
+*.htc: These are the actual behaviors I have used.
+Launcher.htm: Open this file to see a demo application.
+Debug.htm: Provides a DHTML equivalent of TRACE. See the TRACE function in 
+  CommandBars.js.
+Other .htm files: These implement dialog boxes for the demo application.
+*.css: Styles.
+CommandBars.js: Provides global functions for all behaviors.
